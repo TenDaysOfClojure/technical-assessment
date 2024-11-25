e78120493f23d74d23cdbde2aabc3672b6ca29d2
Guilio Karadanais <guilio@simply.co.za>
2024-11-25 19:47:15 +0200

    feat: Integration Tailwind CSS project

---
+++ web-app/css/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./../resources/public/static/**/*.{html,js,clj,cljs,edn}",
+            "./../src/technical_assessment/ux/**/*.{html,js,clj,cljs,edn}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
