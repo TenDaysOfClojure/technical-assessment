7dc226d22b97b1464be2cb2a0e60777147474554
Guilio Karadanais <guilio@simply.co.za>
2024-11-26 06:41:57 +0200

    feat: Add HTTP request logging for visibility and pro-active debugging

--- src/technical_assessment/http_server/middleware.clj
+++ src/technical_assessment/http_server/middleware.clj
@@ -1,7 +1,8 @@
 (ns technical-assessment.http-server.middleware
-  (:require [taoensso.timbre :as logger]
-            [technical-assessment.http-server.render-html :as html]
-            [technical-assessment.ux.pages.general :as general-pages]))
+  (:require [technical-assessment.http-server.render-html :as html]
+            [technical-assessment.ux.pages.general :as general-pages]
+            [taoensso.timbre :as timbre-logger]
+            [ring.logger.protocols :refer [Logger]]))
 
 
 (defn wrap-exceptions [handler]
@@ -11,8 +12,34 @@
       (handler request)
 
       (catch Exception exception
-        (logger/error "Unexpected HTTP server error" {} exception)
+        (timbre-logger/error "Unexpected HTTP server error occurred during request"
+                             {:uri (:uri request)}
+                             exception)
 
         {:status 500
          :body (html/render
                 (general-pages/unexpected-error-page))}))))
+
+
+(def logger-keys-to-redact #{"x-api-key"
+                             "api-key"
+                             "password"
+                             "email-address"
+                             "username"
+                             "Authorization"
+                             "authorization-token"
+                             "user-authorisation-token"
+                             "code"})
+
+;; Defines a custom request logger that creates spacious logging by putting a newline
+;; between logging outputs to make it slighlty more readable
+(defrecord WebRequestLogger []
+  Logger
+
+  (add-extra-middleware [_ handler] handler)
+
+  (log [_ level throwable message]
+    (timbre-logger/log level throwable message)
+
+    #_(.write *out* "\n")
+    (flush)))
