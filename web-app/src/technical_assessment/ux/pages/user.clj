49f3e457a4de74b704b178a1aa88aac4e74bc1fb
TenDaysOfClojure <tendaysofclojure@gmail.com>
2024-12-02 16:44:52 +0200

    feat: User dashboard first draft with field inforation

--- web-app/src/technical_assessment/ux/pages/user.clj
+++ web-app/src/technical_assessment/ux/pages/user.clj
@@ -2,7 +2,17 @@
   (:require [technical-assessment.ux.layouts :as layouts]
             [technical-assessment.urls :as urls]
             [technical-assessment.ux.svg-library :as svg-library]
-            [technical-assessment.domain.user :as domain.user]))
+            [technical-assessment.domain.user :as domain.user]
+            [clojure.pprint]))
+
+(defn user-fields-table [user]
+  ;; Sorts the fields by the key of the map
+  (let [fields (sort-by key user)]
+    [:table.table-auto
+     (for [[key value] fields]
+       [:tr.border.border-slate-200
+        [:td.p-2.text-gray-700.font-semibold (str key)]
+        [:td.p-2.text-gray-500 value]])]))
 
 (defn dashboard-page [user]
   (layouts/main-layout
@@ -21,12 +31,18 @@
 
       (when (domain.user/has-email-address? user)
         (let [email-address (domain.user/email-address user)]
-          [:div
+          [:div.flex.space-x-2
            [:a.link.flex {:href (str "mailto:" email-address)}
-            svg-library/email-icon  email-address]]))]]
+            svg-library/email-icon  email-address]
 
-    [:div
+           [:span " | "]
+
+           [:a.link {:href urls/home-route} "Log out now"]]))]]
+
+    [:div.space-y-3
      [:h2.text-xl "Congratulations, you're now logged in!"]
-     [:p "This completes the technical assessment, thank you for your time."]]
+     [:p "As part of this technical assessment the following user data was persisted:"]
 
-    [:a.link {:href urls/home-route} "Log out now"]]))
+     [:div (user-fields-table user)]
+
+     [:div [:a.link {:href urls/home-route} "Log out now"]]]]))
